37cf41891f372c521dea53edaa04a588b592ffb7
Sudheer Chilumula <sudheer.chilumula@pgi.com>
2018-04-03 11:52:29 -0400

    Test case fix

--- src/app/services/image.service.spec.ts
+++ src/app/services/image.service.spec.ts
@@ -3,30 +3,31 @@ import { async, TestBed } from '@angular/core/testing';
 
 
 describe('Image Service', () => {
-  let service: ImageService;
+    let service: ImageService;
     let imagesMock = [
         {
-        src: 'assets/img/caillou.png',
-        rows: 1,
-        cols: 1,
-        state: 'default'
+            src: 'assets/img/caillou.png',
+            rows: 1,
+            cols: 1,
+            state: 'default'
         },
         {
-        src: 'assets/img/buzz_light_year.png',
-        rows: 2,
-        cols: 2,
-        state: 'clicked'
+            src: 'assets/img/buzz_light_year.png',
+            rows: 2,
+            cols: 2,
+            state: 'clicked'
         }
     ]
-  beforeEach(
-    async(() => {
-      TestBed.configureTestingModule({
-        providers: [ImageService],
-      });
-      service = TestBed.get(ImageService);
-    })
-  );
+    beforeEach(
+        async(() => {
+            TestBed.configureTestingModule({
+                providers: [ImageService],
+            });
+            service = TestBed.get(ImageService);
+        })
+    );
 
-  it('should have a service', () => {
-    expect(service).toBeDefined();
-  });
+    it('should have a service', () => {
+        expect(service).toBeDefined();
+    });
+});  
